a23767cd4d5e70f9d66079677db39f2c6bb445ca
Alsu1990 <alexsukmanov1990@gmail.com>
2021-12-03 13:30:55 +0200

    i2s_gen complete

---
+++ ANC_LMS_NexysA7.srcs/sources_1/new/PDM_MIC_INT.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/20/2021 07:18:12 PM
+// Design Name: 
+// Module Name: PDM_MIC_INT
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module PDM_MIC_INT(
+
+    );
+endmodule
